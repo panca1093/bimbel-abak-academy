cc6f4c0efa07b6fe995d1b3e4203f6111aa53960
Saifullah Panca <panggilsaya.saiful@gmail.com>
2026-07-06 11:58:05 +0700

    feat: remove unused Bell import from notifications page

--- web/app/(admin)/admin/notifications/page.tsx
+++ web/app/(admin)/admin/notifications/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Bell } from "lucide-react";
 import { AnnouncementTable } from "@/components/admin/AnnouncementTable";
 import { AnnouncementComposer } from "@/components/admin/AnnouncementComposer";
 import { PurchaseNotificationFeed } from "@/components/admin/PurchaseNotificationFeed";
